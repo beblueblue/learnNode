aae90e05323e1990e0b8f484e54f70b7c2a2d976
hhz <860601963@qq.com>
2017-05-17 16:16:49 +0800

    add koa-router in app.js

--- url-koa/app.js
+++ url-koa/app.js
@@ -4,6 +4,9 @@ const Koa = require('koa');
 //注意require('koa-router')返回的是函数：
 const router = require('koa-router')();
 
+//引入"koa-bodyparser":
+const bodyParser = require('koa-bodyparser');
+
 //创建一个Koa对象表示web app本身
 const app = new Koa();
 
@@ -13,7 +16,7 @@ app.use(async(ctx,next)=>{
     await next();
 });
 
-//增加地址路由 add url-route：
+/* //增加地址路由 add url-route：
 router.get('/hello/:name',async(ctx,next)=>{
     var name= ctx.params.name;
     ctx.response.body = `<h1>Hello, ${name}!</h1>`
@@ -21,7 +24,33 @@ router.get('/hello/:name',async(ctx,next)=>{
 
 router.get('/',async(ctx,next)=>{
     ctx.response.body = '<h1>Index</h1>';
+}); */
+
+//简单的登录表单：
+router.get('/',async(ctx,next)=>{
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
 });
+
+router.post('/signin',async(ctx,next)=>{
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password ||'';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if(name === 'koa' && password === '12345'){
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+            <p><a href="/">Try again</p>`
+    }
+});
+
+//增加koa-bodyparser中间件
+app.use(bodyParser());
 
 //增加路由中间件 add router middleware:
 app.use(router.routes());
