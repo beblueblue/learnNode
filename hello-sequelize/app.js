0b13d1f529f50a6136365611330734530b988cd4
hhz <860601963@qq.com>
2017-06-01 22:20:43 +0800

    try to use the sequelize about mysql in hello-sequelize

---
+++ hello-sequelize/app.js
@@ -0,0 +1,76 @@
+//创建一个sequelize对象实例
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+//定义模型Pet，告诉Suquelize数据库表的映射关系
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+},{
+        timestamps: false
+});
+
+//使用Promise的方式，操作数据库表的数据
+var now = Date.now();
+
+Pet.create({
+    id: 'g-' + now,
+    name: 'Gaffey',
+    gender: false,
+    birth: '2007-07-07',
+    createdAt: now,
+    updatedAt: now,
+    version: 0
+}).then(function (p) {
+    console.log('created.' + JSON.stringify(p));
+}).catch(function (err) {
+    console.log('failed: ' + err);
+});
+
+//使用async...await方式，操作数据库表的数据
+(async ()=> {
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Gaffey'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (let p of pets) {
+        console.log(JSON.stringify(p));
+    }
+})();
+
+/*
+//如果需要更新数据，对查询到的实例调用save()方法：
+(async () => {
+    var p = await queryFromSomewhere();
+    p.gender = true;
+    p.updatedAt = Date.now();
+    p.version ++;
+    await p.save();
+})();
+
+//如果需要删除属性，对查询到的实例调用destroy()方法：
+(async () => {
+    var p = await queryFromSomewhere();
+    await p.destroy();
+})();
+*/
